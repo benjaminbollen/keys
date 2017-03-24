570d6f05e6aa4d2ce0e365fe0aa1e9753f8b4b4b
Benjamin Bollen <ben@monax.io>
2017-03-24 16:05:41 +0100

    common: add scratchpath for tests

--- common/paths.go
+++ common/paths.go
@@ -1,14 +1,48 @@
 package common
 
 import (
+	"os"
+	"runtime"
 	"path/filepath"
-
 )
 
 var (
 	// Convenience directories.
+	MonaxRoot          = ResolveMonaxRoot()
 	MonaxContainerRoot = "/home/eris/.eris"
 
 	// Major directories.
-	KeysPath = filepath.Join(MonaxContainerRoot, "keys")
+	KeysPath    = filepath.Join(MonaxRoot, "keys")
+	ScratchPath = filepath.Join(MonaxRoot, "scratch")
 )
+
+func HomeDir() string {
+	if runtime.GOOS == "windows" {
+		drive := os.Getenv("HOMEDRIVE")
+		path := os.Getenv("HOMEPATH")
+		if drive == "" || path == "" {
+			return os.Getenv("USERPROFILE")
+		}
+		return drive + path
+	} else {
+		return os.Getenv("HOME")
+	}
+}
+
+func ResolveMonaxRoot() string {
+	var eris string
+	if os.Getenv("ERIS") != "" {
+		eris = os.Getenv("ERIS")
+	} else {
+		if runtime.GOOS == "windows" {
+			home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+			if home == "" {
+				home = os.Getenv("USERPROFILE")
+			}
+			eris = filepath.Join(home, ".eris")
+		} else {
+			eris = filepath.Join(HomeDir(), ".eris")
+		}
+	}
+	return eris
+}
